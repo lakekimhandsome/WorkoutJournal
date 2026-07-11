2822ff0d17a4fdbbcb74ecd8bea03a1d9c6aabf2
lakekimhandsome <joshualakekim@gmail.com>
2026-07-11 15:17:11 +0900

    Set up project structure

---
+++ WorkoutJournal/Views/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  WorkoutJournal
+//
+//  Created by 김호수 on 7/11/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Workout Journal")
+    }
+}
+
+#Preview {
+    HomeView()
+}
