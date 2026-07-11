13a783ab4b75a96519b19e14525a7ec6404a2e9b
lakekimhandsome <joshualakekim@gmail.com>
2026-07-11 14:55:51 +0900

    Initial Commit

---
+++ WorkoutJournal/WorkoutJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutJournalApp.swift
+//  WorkoutJournal
+//
+//  Created by 김호수 on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
